d0f059f8499d0650610586ae9d66ad6fdacd6aad
Elijah Melis <elimelis@MacBook-Pro.local>
2020-04-16 22:05:20 -0500

    solved challenge #11

--- closures/index.js
+++ closures/index.js
@@ -239,6 +239,28 @@ const subtract = function(big, small) { return big - small; };
 const subFrom20 = defineFirstArg(subtract, 20);
 console.log(subFrom20(5)); // => should log 15
 
+/**
+ * Challenge 11
+ * Create a function dateStamp that accepts a function and returns a function. 
+ * The returned function will accept however many arguments the passed-in function accepts, 
+ * and return an object with a date key that contains a timestamp with the time of invocation, 
+ * and an output key that contains the result from invoking the passed-in function. 
+ * HINT: You may need to research how to access information on Date objects.
+ */
+
+ function dateStamp(func) {
+	return function(value) {
+    return {
+      date: new Date().toISOString().slice(0, 10), output: func(value)
+    }
+  };
+}
+
+// /*** Uncomment these to check your work! ***/
+const stampedMultBy2 = dateStamp(n => n * 2);
+console.log(stampedMultBy2(4)); // => should log { date: (today's date), output: 8 }
+console.log(stampedMultBy2(6)); // => should log { date: (today's date), output: 12 }
+
 
 
 /*
