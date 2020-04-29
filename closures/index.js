fa10c822e404be2ccf804d08199e1ded29272e2a
Elijah Melis <elimelis@MacBook-Pro.local>
2020-04-28 21:47:22 -0500

    solve challenge 15

--- closures/index.js
+++ closures/index.js
@@ -347,6 +347,34 @@ function censor() {
   obj.setSecret(2)
   console.log(obj.getSecret()) // => returns 2
 
+  /**
+   * Challenge 15
+   * Create a function russianRoulette that accepts a number (let us call it n), and returns a function. 
+   * The returned function will take no arguments, and will return the string 'click' the first n - 1 number of times it is invoked. 
+   * On the very next invocation (the nth invocation), the returned function will return the string 'bang'. 
+   * On every invocation after that, the returned function returns the string 'reload to play again'.
+   */
+
+  function russianRoulette(num) {
+    return function() {
+      num--;
+      if(num === 0) {
+        return 'bang';
+      } else if(num > 0) {
+        return 'click';
+      } else {
+        return 'reload to play again';
+      }
+    }
+  }
+  
+  // /*** Uncomment these to check your work! ***/
+  const play = russianRoulette(3);
+  console.log(play()); // => should log 'click'
+  console.log(play()); // => should log 'click'
+  console.log(play()); // => should log 'bang'
+  console.log(play()); // => should log 'reload to play again'
+  console.log(play()); // => should log 'reload to play again'
 
 /*
     Other challanges
